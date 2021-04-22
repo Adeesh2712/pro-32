bb4847726ef94addec96cec5643b7532dfb6a4f0
Adeesh2712 <76524701+Adeesh2712@users.noreply.github.com>
2021-04-22 18:38:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,95 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+   getBackgroundImg();
+    
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg)
+    background(backgroundImg);
+    // add condition to check if any background image is there to add
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    //change the data in JSON format
+    var datetime = responseJSON.datetime;
+    
+
+    // write code slice the datetime
+    var hour = datetime.slice(11,13);
+
+    console.log(hour)
+    // add conditions to change the background images from sunrise to sunset
+    if(hour>=04 && hour<06){
+        bg = "sunrise1.png";
+    }
+    else if(hour>=06 && hour<08){
+        bg = "sunrise2.png";
+    }
+    else if(hour>=08 && hour<10){
+        bg = "sunrise3.png";
+    }
+    else if(hour>=10 && hour<12){
+        bg = "sunrise4.png";
+    }
+    else if(hour>=12 && hour<14){
+        bg = "sunrise5.png";
+    }
+    else if(hour>=14 && hour<16){
+        bg = "sunset6.png";
+    }
+    else if(hour>=16 && hour<18){
+        bg = "sunrise7.png";
+    }
+    else if(hour>=18 && hour<20){
+        bg = "sunset8.png";
+    }
+    else if(hour>=20 && hour<22){
+        bg = "sunset9.png";
+    }
+    else if(hour>=22 && hour<24){
+        bg = "sunset12.png";
+    }
+    else if(hour>=24 && hour<2){
+        bg = "sunset11.png";
+    }
+    else{
+        bg = "sunset12.png";
+    }
+    
+    
+    
+
+    //load the image in backgroundImg variable here
+    
+
+    backgroundImg = loadImage(bg);
+}
